bfb0a7d65f7423c2b37c6d1a234e1791f479d4a3
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2019-10-16 23:08:46 +0300

    Implemented simple data fetch from github

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import axios from "axios";
 import {
   Table,
   TableHead,
@@ -10,41 +11,75 @@ import {
 } from "../components/dynoTable";
 import "./App.scss";
 
-function App() {
-  useEffect(() => {
-    // Implement fetch logic;
-  }, []);
-  return (
-    <div className="App">
-      <Table>
-        <TableHead>
-          <HeadRow>
-            <HeadColumn onSortChange={handleSort}>Name</HeadColumn>
-            <HeadColumn onSortChange={handleSort}>Age</HeadColumn>
-            <HeadColumn onSortChange={handleSort}>Country</HeadColumn>
-            <HeadColumn onSortChange={handleSort}>Greeting</HeadColumn>
-          </HeadRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableColumn>Test1</TableColumn>
-            <TableColumn>Test2</TableColumn>
-            <TableColumn>Test2</TableColumn>
-            <TableColumn>Test2</TableColumn>
-          </TableRow>
-          <TableRow>
-            <TableColumn>Test1</TableColumn>
-            <TableColumn>Test2</TableColumn>
-            <TableColumn>Test2</TableColumn>
-            <TableColumn>Test2</TableColumn>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
-  );
+const CATS_API = "https://raw.githubusercontent.com/viktornar/dynotable/master/data/cats.json";
 
-  function handleSort(sortOrder) {
-    console.log(sortOrder);
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoading: false,
+      cats: []
+    };
+
+    this.handleSort = this.handleSort.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    axios
+      .get(
+        CATS_API
+      )
+      .then(({ data: { cats } }) => {
+        this.setState({ cats });
+        this.setState({ isLoading: false });
+      })
+      .catch(error => {
+        this.setState({ error, isLoading: false });
+      });
+  }
+
+  handleSort(sortOrder) {
+    // TODO: Implement sorting that can sort a big amount of data
+  }
+
+  render() {
+    const { isLoading, cats } = this.state;
+    return (
+      <div className="App">
+        <Table>
+          <TableHead>
+            <HeadRow>
+              <HeadColumn onSortChange={this.handleSort}>Name</HeadColumn>
+              <HeadColumn onSortChange={this.handleSort}>Age</HeadColumn>
+              <HeadColumn onSortChange={this.handleSort}>Country</HeadColumn>
+              <HeadColumn onSortChange={this.handleSort}>Greeting</HeadColumn>
+            </HeadRow>
+          </TableHead>
+          <TableBody>
+            {isLoading ? (
+              <div className="App__loader">Loading...</div>
+            ) : (
+              <>
+                <TableRow>
+                  <TableColumn>Test1</TableColumn>
+                  <TableColumn>Test2</TableColumn>
+                  <TableColumn>Test2</TableColumn>
+                  <TableColumn>Test2</TableColumn>
+                </TableRow>
+                <TableRow>
+                  <TableColumn>Test1</TableColumn>
+                  <TableColumn>Test2</TableColumn>
+                  <TableColumn>Test2</TableColumn>
+                  <TableColumn>Test2</TableColumn>
+                </TableRow>
+              </>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    );
   }
 }
 
---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+export function useCatsFetcher() {
+  const cats = [];
+  const isLoading = false;
+  const isError = false;
+  return [cats, isLoading, isError];
+}
