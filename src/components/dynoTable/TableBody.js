6ed9755452f08a8bf99819d7d48d3f5ebb4fb308
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2019-10-16 21:07:48 +0300

    Added styling prototype for table body and head cells

--- src/components/dynoTable/TableBody.js
+++ src/components/dynoTable/TableBody.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React from 'react';
+import './TableBody.scss';
 
 export default function TableBody({ children }) {
-  return <div className="flexContainer">{children}</div>;
+  return <div className="flexContainer TableBody">{children}</div>;
 }
--- src/components/dynoTable/TableHead.js
+++ src/components/dynoTable/TableHead.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './TableHead.scss';
 
 export default function TableHead({children}) {
   return (
-    <div className="flexContainer">
+    <div className="flexContainer TableHead">
       {children}
     </div>
   )
