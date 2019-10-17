8b47ebeb47e9c7cdabb6944336a4d9d26a04381a
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2019-10-18 01:19:49 +0300

    Updates

---
+++ precache-manifest.8ba78b9c0d0a1abedcf61ee48f448ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f09c2d95258a62a0b91cd00ae48947",
+    "url": "/dynotable/index.html"
+  },
+  {
+    "revision": "3a1c3ebe693495eef44d",
+    "url": "/dynotable/static/css/main.e575d945.chunk.css"
+  },
+  {
+    "revision": "2d0f7ba970bc1b556975",
+    "url": "/dynotable/static/js/2.ab3ee0a4.chunk.js"
+  },
+  {
+    "revision": "3a1c3ebe693495eef44d",
+    "url": "/dynotable/static/js/main.8ad79da0.chunk.js"
+  },
+  {
+    "revision": "5aef8f7145bcf0262bac",
+    "url": "/dynotable/static/js/runtime-main.93443573.js"
+  }
+]);
